371c14e2a5d0c3ccadd7b101be2fe38ac85232f5
ervan19 <ervanmuhammada454@gmail.com>
2023-07-29 15:42:18 +0700

    fix(certificate): fixing bug in certificate section

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -55,13 +55,6 @@ const Carousel = () => {
           className='mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
           aria-label='Slide 5'
         ></button>
-        <button
-          type='button'
-          data-te-target='#carouselExampleCaptions'
-          data-te-slide-to='5'
-          className='mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none'
-          aria-label='Slide 6'
-        ></button>
       </div>
 
       <div className="relative w-full md:w-8/12 mx-auto overflow-hidden after:clear-both after:block after:content-['']">
