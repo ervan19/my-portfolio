67c4f88b4d1587fa19e34837a45fe6d9403aa4a1
ervanmuhammada <ervanmuhammada454@gmail.com>
2024-05-14 01:20:19 +0700

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useCallback, useMemo, useState } from 'react';
-import ThemeContext from './context/theme';
-import Layout from './layout/Layout';
-import WebProjectDetail from './pages/WebProjectDetail';
-import UiProjectDetail from './pages/UiProjectDetail';
-import { Route, Routes } from 'react-router-dom';
-import AOS from 'aos';
-import Home from './pages/Home';
+import { useEffect, useCallback, useMemo, useState } from "react";
+import ThemeContext from "./context/theme";
+import Layout from "./layout/Layout";
+import WebProjectDetail from "./pages/WebProjectDetail";
+import UiProjectDetail from "./pages/UiProjectDetail";
+import { Route, Routes } from "react-router-dom";
+import AOS from "aos";
+import Home from "./pages/Home";
 
-import { initTE, Carousel } from 'tw-elements';
+import { initTE, Carousel } from "tw-elements";
 
 function App() {
-  const [dark, setDark] = useState('dark');
+  const [dark, setDark] = useState("dark");
 
   useEffect(() => {
     AOS.init({
@@ -18,22 +18,22 @@ function App() {
       offset: 120, // offset (in px) from the original trigger point
       delay: 0, // values from 0 to 3000, with step 50ms
       duration: 1000, // values from 0 to 3000, with step 50ms
-      easing: 'ease', // default easing for AOS animations
+      easing: "ease", // default easing for AOS animations
       once: false, // whether animation should happen only once - while scrolling down
     });
     initTE({ Carousel });
-    document.body.setAttribute('theme', dark);
-    localStorage.setItem('theme', JSON.stringify(dark));
+    document.body.setAttribute("theme", dark);
+    localStorage.setItem("theme", JSON.stringify(dark));
   }, [dark]);
 
   const toggleDark = useCallback(() => {
-    if (dark === 'light') {
-      setDark('dark');
+    if (dark === "light") {
+      setDark("dark");
     } else {
-      setDark('light');
+      setDark("light");
     }
 
-    localStorage.setItem('theme', JSON.stringify(dark));
+    localStorage.setItem("theme", JSON.stringify(dark));
   }, [dark]);
 
   const providerValue = useMemo(
@@ -41,16 +41,16 @@ function App() {
       dark,
       toggleDark,
     }),
-    [dark, toggleDark],
+    [dark, toggleDark]
   );
 
   return (
     <ThemeContext.Provider value={providerValue}>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/studycase/web/:id' element={<WebProjectDetail />} />
-          <Route path='/studycase/ui/:id' element={<UiProjectDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/studycase/web/:id" element={<WebProjectDetail />} />
+          <Route path="/studycase/ui/:id" element={<UiProjectDetail />} />
         </Route>
       </Routes>
     </ThemeContext.Provider>
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { registerSW } from 'virtual:pwa-register';
-import App from './App.jsx';
-import './styles/index.css';
-import 'aos/dist/aos.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
+import { Analytics } from "@vercel/analytics/react";
+import App from "./App.jsx";
+import "./styles/index.css";
+import "aos/dist/aos.css";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm('New content available. Reload?')) {
+    if (confirm("New content available. Reload?")) {
       updateSW(true);
     }
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
